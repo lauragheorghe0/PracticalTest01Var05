2f5ecbc83f1ed8892148f7580a85f385fbe4561c
Laura Gheorghe <gh.laura.elena@gmail.com>
2022-04-11 08:37:19 +0300

    Add GUI to app (A2)

--- PracticalTest01Var05/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/PracticalTest01Var05MainActivity.java
+++ PracticalTest01Var05/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/PracticalTest01Var05MainActivity.java
@@ -3,8 +3,25 @@ package ro.pub.cs.systems.eim.practicaltest01var05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PracticalTest01Var05MainActivity extends AppCompatActivity {
+    private Button centerButton;
+    private Button topLeftButton;
+    private Button topRightButton;
+    private Button bottomLeftButton;
+    private Button bottomRightButton;
+
+    private RelativeButtonOnClickListener relativeButtonOnClickListener = new RelativeButtonOnClickListener();
+    private class RelativeButtonOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
